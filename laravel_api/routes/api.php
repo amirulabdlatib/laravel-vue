d8b3f589566d3c668993037f5a5d32cceeb79c43
Amirul <amirulal73@gmail.com>
2025-07-18 17:55:27 +0800

    Add task complete feature

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\CompleteTaskController;
 use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,4 +12,5 @@ Route::get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function(){
     Route::apiResource('tasks',TaskController::class);
+    Route::patch('/tasks/{tasks}/complete',CompleteTaskController::class);
 });
