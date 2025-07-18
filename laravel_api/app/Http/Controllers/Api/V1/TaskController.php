ba840b467fe9937cb35da324258eb014717c8378
Amirul <amirulal73@gmail.com>
2025-07-18 17:56:53 +0800

    Add delete task functionality

--- laravel_api/app/Http/Controllers/Api/V1/TaskController.php
+++ laravel_api/app/Http/Controllers/Api/V1/TaskController.php
@@ -51,8 +51,10 @@ class TaskController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 }
