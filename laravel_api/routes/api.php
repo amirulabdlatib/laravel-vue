6bd03c604738607a8dcd63782dfeeb984d13b8f8
Amirul <amirulal73@gmail.com>
2025-07-18 21:07:09 +0800

    Add logout feature - Token based sanctum

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\V1\CompleteTaskController;
 use App\Http\Controllers\Api\V1\TaskController;
@@ -20,4 +21,5 @@ Route::prefix('v1')->group(function(){
 Route::prefix('auth')->group(function(){
     Route::post('/register',RegisterController::class);
     Route::post('/login',LoginController::class);
+    Route::post('/logout',LogoutController::class)->middleware('auth:sanctum');
 });
